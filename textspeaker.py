99688ba7cfab231077d79a4b985f93ef30aa39f8
Saurav Pradhan <saurav.pradhan54@gmail.com>
2020-05-10 20:23:55 +1000

    Changing to class

--- textspeaker.py
+++ textspeaker.py
@@ -1,11 +1,19 @@
 from google_speech import Speech
 
-# say "Hello World"
-text = input("Please enter your text")
-lang = "en"
-speech = Speech(text, lang)
 
-# you can also apply audio effects while playing (using SoX)
-# see http://sox.sourceforge.net/sox.html#EFFECTS for full effect documentation
-sox_effects = ("tempo", "1.5")
-speech.play(sox_effects)
+class TextReader:
+
+    def __init__(self, data):
+        self.data = data
+
+    
+    def speak(self):
+        lang = "en"
+        speech = Speech(self.data, lang)
+        
+        # you can also apply audio effects while playing (using SoX)
+        # see http://sox.sourceforge.net/sox.html#EFFECTS for full effect documentation
+        sox_effects = ("tempo", "1.5")
+        speech.play(sox_effects)
+
+
